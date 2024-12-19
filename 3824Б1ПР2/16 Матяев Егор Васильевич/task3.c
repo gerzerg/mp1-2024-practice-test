9545d86932eabf6581b2695f83bde3b7154b7f2a
gerzerg <15.03.2007e@gmail.com>
2024-12-19 10:19:46 +0300

    Update task3.c

--- 3824Б1ПР2/16 Матяев Егор Васильевич/task3.c
+++ 3824Б1ПР2/16 Матяев Егор Васильевич/task3.c
@@ -1,6 +1,38 @@
-// Задача 3. Дана строка s, состоящая из английских слов, разделенных пробелами
-// (одним или несколькими). Определить количество слов в строке.
-int task3(char *s)
-{
-  return -1;
+#include <stdio.h>
+#include <ctype.h>
+
+int task3(char* s) {
+    if (s == NULL || *s == '0') { 
+        return -1;
+    }
+
+    int count = 0; 
+    int in_word = 0; 
+
+    while (*s) { 
+        if (isspace(*s)) { 
+            in_word = 0; 
+        }
+        else if (!in_word) { 
+            in_word = 1; 
+            count++; 
+        }
+        s++;
+    }
+
+    return count;
+}
+
+int main() {
+    char str[] = " Hello   world! This is a test string for task. ";
+    int result = task3(str);
+
+    if (result == -1) {
+        printf("Строка пуста.\n");
+    }
+    else {
+        printf("Количество слов в строке: %d\n", result);
+    }
+
+    return 0;
 }
