44d20b5473cdab1b6a998de3a111e87b2e0f9593
gerzerg <15.03.2007e@gmail.com>
2024-12-19 09:48:46 +0300

    Update task2.c

--- 3824Б1ПР2/16 Матяев Егор Васильевич/task2.c
+++ 3824Б1ПР2/16 Матяев Егор Васильевич/task2.c
@@ -1,8 +1,33 @@
-// Задача 2. Дан непустой массив A, содержащий n натуральных чисел.
-// Назовем «серией» группу подряд идущих одинаковых элементов, 
-// а длиной серии – количество этих элементов (длина серии может быть равна 2).
-// Найти число серий в массиве.
-int task2(unsigned A[], size_t n)
-{
-  return -1;
+#include <stdio.h>
+
+int task2(unsigned A[], size_t n) {
+    if (n == 0) {
+        return -1; 
+    }
+
+    int series_cnt = 1; 
+
+    
+    for (size_t i = 1; i < n; i++) {
+        if (A[i] != A[i - 1]) { 
+            series_cnt++; 
+        }
+    }
+    return series_cnt; 
+}
+
+int main() {
+    unsigned A[] = { 1, 1, 2, 2, 3, 3, 3, 4, 5 };
+    size_t n = sizeof(A) / sizeof(A[0]);
+
+    int result = task2(A, n);
+
+    if (result == -1) {
+        printf("Массив пустой.\n");
+    }
+    else {
+        printf("Количество серий в массиве: %d\n", result);
+    }
+
+    return 0;
 }
